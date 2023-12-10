7968ab853986242b23ef65046e354b0ceb1c09c5
[treasure3210] <treasure3210@gmail.com>
2023-12-09 21:46:25 -0700

    Apply tabView to the main app along with onboarding as sheet

--- ExpenseTracker/ContentView.swift
+++ ExpenseTracker/ContentView.swift
@@ -8,14 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    /// Intro Visibility Status
+    @AppStorage("isFirstTime") private var isFirstTime = true
+    /// Active Tab
+    @State private var activeTab: Tab = .recents
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView(selection: $activeTab) {
+            Text(Tab.recents.rawValue)
+                .tag(Tab.recents)
+                .tabItem { Tab.recents.tabContent }
+            
+            Text(Tab.search.rawValue)
+                .tag(Tab.search)
+                .tabItem { Tab.search.tabContent }
+            
+            Text(Tab.charts.rawValue)
+                .tag(Tab.charts)
+                .tabItem { Tab.charts.tabContent }
+            
+            Text(Tab.settings.rawValue)
+                .tag(Tab.settings)
+                .tabItem { Tab.settings.tabContent }
         }
-        .padding()
+        .sheet(isPresented: $isFirstTime, content: {
+            IntroScreen()
+                .interactiveDismissDisabled()
+        })
     }
 }
 
