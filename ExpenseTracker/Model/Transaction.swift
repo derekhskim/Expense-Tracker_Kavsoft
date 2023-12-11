269c59dd0abe78e547b891f3a39fb09c29119061
[treasure3210] <treasure3210@gmail.com>
2023-12-10 23:45:02 -0700

    Apply tintColor

--- ExpenseTracker/Model/Transaction.swift
+++ ExpenseTracker/Model/Transaction.swift
@@ -17,12 +17,17 @@ struct Transaction: Identifiable {
     var category: String
     var tintColor: String
     
-    init(title: String, remarks: String, amount: Double, dateAdded: Date, category: Category, tintColor: String) {
+    init(title: String, remarks: String, amount: Double, dateAdded: Date, category: Category, tintColor: TintColor) {
         self.title = title
         self.remarks = remarks
         self.amount = amount
         self.dateAdded = dateAdded
         self.category = category.rawValue
-        self.tintColor = tintColor
+        self.tintColor = tintColor.color
+    }
+    
+    /// Extracting Color Value from tintColor String
+    var color: Color {
+        return tints.first(where: { $0.color == tintColor })?.value ?? appTint
     }
 }
