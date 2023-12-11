c8a0b7f6ece0cce6e5d733e34efce04646f41d5c
[treasure3210] <treasure3210@gmail.com>
2023-12-10 22:35:37 -0700

    Create a model for tintColor

---
+++ ExpenseTracker/Model/Tintcolor.swift
@@ -0,0 +1,24 @@
+//
+//  Tintcolor.swift
+//  ExpenseTracker
+//
+//  Created by Derek Kim on 2023-12-10.
+//
+
+import SwiftUI
+
+/// Custom Tint Colors For Transaction Row
+struct TintColor: Identifiable {
+    let id: UUID = .init()
+    var color: String
+    var value: Color
+}
+
+var tints: [TintColor] = [
+    .init(color: "Red", value: .red),
+    .init(color: "Blue", value: .blue),
+    .init(color: "Pink", value: .pink),
+    .init(color: "Purple", value: .purple),
+    .init(color: "Brown", value: .brown),
+    .init(color: "Orange", value: .orange),
+]
