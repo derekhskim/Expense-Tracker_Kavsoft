8f63682758b460cfb2476d7d0072deabddc2ffab
[treasure3210] <treasure3210@gmail.com>
2023-12-10 22:34:54 -0700

    Create Category enum

---
+++ ExpenseTracker/Model/Category.swift
@@ -0,0 +1,13 @@
+//
+//  Category.swift
+//  ExpenseTracker
+//
+//  Created by Derek Kim on 2023-12-10.
+//
+
+import SwiftUI
+
+enum Category: String {
+    case income = "Income"
+    case expense = "Expense"
+}
