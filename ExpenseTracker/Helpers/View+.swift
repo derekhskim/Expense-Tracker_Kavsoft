f9b345567381ff969e0d0f5d2ac6b0f4e367bd63
[treasure3210] <treasure3210@gmail.com>
2023-12-11 18:27:20 -0700

    Create custom modifier for repetitive use

---
+++ ExpenseTracker/Helpers/View+.swift
@@ -0,0 +1,22 @@
+//
+//  View+.swift
+//  ExpenseTracker
+//
+//  Created by Derek Kim on 2023-12-11.
+//
+
+import SwiftUI
+
+extension View {
+    @ViewBuilder
+    func hSpacing(_ alignment: Alignment = .center) -> some View {
+        self
+            .frame(maxWidth: .infinity, alignment: alignment)
+    }
+    
+    @ViewBuilder
+    func vSpacing(_ alignment: Alignment = .center) -> some View {
+        self
+            .frame(maxHeight: .infinity, alignment: alignment)
+    }
+}
