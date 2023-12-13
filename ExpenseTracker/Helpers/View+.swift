0c46df41b42d03d2b5d0b249431339c6289a3ac1
[treasure3210] <treasure3210@gmail.com>
2023-12-12 22:38:36 -0700

    Create formatter input method for Date

--- ExpenseTracker/Helpers/View+.swift
+++ ExpenseTracker/Helpers/View+.swift
@@ -27,4 +27,10 @@ extension View {
         
         return .zero
     }
+    
+    func format(date: Date, format: String) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = format
+        return formatter.string(from: date)
+    }
 }
