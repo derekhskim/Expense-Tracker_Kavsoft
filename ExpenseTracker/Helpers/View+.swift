1ec1920220a9cc8c95659ee5a891093dbd020cb7
[treasure3210] <treasure3210@gmail.com>
2023-12-13 00:02:39 -0700

    Create currency String input method

--- ExpenseTracker/Helpers/View+.swift
+++ ExpenseTracker/Helpers/View+.swift
@@ -33,4 +33,12 @@ extension View {
         formatter.dateFormat = format
         return formatter.string(from: date)
     }
+    
+    func currencyString(_ value: Double, allowedDigits: Int = 2) -> String {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.maximumFractionDigits = allowedDigits
+        
+        return formatter.string(from: .init(value: value)) ?? ""
+    }
 }
