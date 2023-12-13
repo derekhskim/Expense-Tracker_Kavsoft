867f3543109727811e30345cb517d0109e16c000
[treasure3210] <treasure3210@gmail.com>
2023-12-12 18:51:39 -0700

    Create an extension for Date to get start & end of month

---
+++ ExpenseTracker/Helpers/Date+.swift
@@ -0,0 +1,23 @@
+//
+//  Date+.swift
+//  ExpenseTracker
+//
+//  Created by Derek Kim on 2023-12-12.
+//
+
+import SwiftUI
+
+extension Date {
+    var startOfMonth: Date {
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.year, .month], from: self)
+        
+        return calendar.date(from: components) ?? self
+    }
+
+    var endOfMonth: Date {
+        let calendar = Calendar.current
+        
+        return calendar.date(byAdding: .init(month: 1, minute: -1), to: self.startOfMonth) ?? self
+    }
+}
