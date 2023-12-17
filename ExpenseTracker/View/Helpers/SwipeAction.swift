af1c7e08fc68fc8c324a61071d368d55413f26a1
[treasure3210] <treasure3210@gmail.com>
2023-12-16 22:48:22 -0700

    Create Action Model for swipe action button options

--- ExpenseTracker/View/Helpers/SwipeAction.swift
+++ ExpenseTracker/View/Helpers/SwipeAction.swift
@@ -25,6 +25,17 @@ struct SwipeAction<Content: View>: View {
     }
 }
 
+/// Action Model
+struct Action: Identifiable {
+    private(set) var id: UUID = .init()
+    var tint: Color
+    var icon: String
+    var iconFont: Font = .title
+    var iconTint: Color = .white
+    var isEnabled: Bool = true
+    var action: () -> ()
+}
+
 #Preview {
     SwipeAction()
 }
