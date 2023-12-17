801a02968949f005bfedd4b747fc3e57198d110b
[treasure3210] <treasure3210@gmail.com>
2023-12-17 12:19:05 -0700

    Create a custom transition

---
+++ ExpenseTracker/View/Helpers/CustomTransition.swift
@@ -0,0 +1,23 @@
+//
+//  CustomTransition.swift
+//  ExpenseTracker
+//
+//  Created by Derek Kim on 2023-12-17.
+//
+
+import SwiftUI
+
+struct CustomTransition: Transition {
+    func body(content: Content, phase: TransitionPhase) -> some View {
+        content
+            .mask {
+                GeometryReader {
+                    let size = $0.size
+                    
+                    Rectangle()
+                        .offset(y: phase == .identity ? 0 : -size.height)
+                }
+                .containerRelativeFrame(.horizontal)
+            }
+    }
+}
