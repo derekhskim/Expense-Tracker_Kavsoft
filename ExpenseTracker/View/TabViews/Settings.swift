681b5fba28714f466f20353a469886cb3d3a283a
[treasure3210] <treasure3210@gmail.com>
2023-12-19 23:15:44 -0700

    Create general username field and lock app toggle

--- ExpenseTracker/View/TabViews/Settings.swift
+++ ExpenseTracker/View/TabViews/Settings.swift
@@ -8,11 +8,32 @@
 import SwiftUI
 
 struct Settings: View {
+    /// User Properties
+    @AppStorage("userName") private var userName: String = ""
+    /// App Lock Properties
+    @AppStorage("isAppLockEnabled") private var isAppLockEnabled: Bool = false
+    @AppStorage("lockWhenAppGoesBackground") private var lockWhenAppGoesBackground: Bool = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            List {
+                Section("User Name") {
+                    TextField("Enter your username", text: $userName)
+                }
+                
+                Section("App Lock") {
+                    Toggle("Enable App Lock", isOn: $isAppLockEnabled)
+                    
+                    if isAppLockEnabled {
+                        Toggle("Lock When App Goes Background", isOn: $lockWhenAppGoesBackground)
+                    }
+                }
+                
+            }
+            .navigationTitle("Settings")
+        }
     }
 }
 
 #Preview {
-    Settings()
+    ContentView()
 }
