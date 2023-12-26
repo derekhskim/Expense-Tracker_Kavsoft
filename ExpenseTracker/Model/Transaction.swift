0b7f694a2b99b8cdd6dc36fab762185722cea34c
[treasure3210] <treasure3210@gmail.com>
2023-12-25 18:41:59 -0700

    Refactor to use SwiftData

--- ExpenseTracker/Model/Transaction.swift
+++ ExpenseTracker/Model/Transaction.swift
@@ -6,9 +6,10 @@
 //
 
 import SwiftUI
+import SwiftData
 
-struct Transaction: Identifiable {
-    let id: UUID = .init()
+@Model
+class Transaction: Identifiable {
     /// Properties
     var title: String
     var remarks: String
@@ -31,9 +32,3 @@ struct Transaction: Identifiable {
         return tints.first(where: { $0.color == tintColor })?.value ?? appTint
     }
 }
-
-var sampleTransaction: [Transaction] = [
-    .init(title: "Magic Keyboard", remarks: "Apple Product", amount: 129, dateAdded: .now, category: .expense, tintColor: tints.randomElement()!),
-    .init(title: "Payroll", remarks: "Company Direct Deposit", amount: 2000, dateAdded: .now, category: .income, tintColor: tints.randomElement()!),
-    .init(title: "Apple Music", remarks: "Subscription", amount: 10.99, dateAdded: .now, category: .expense, tintColor: tints.randomElement()!),
-]
