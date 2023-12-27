02f21e3b4c12297d4a6e0ee8696ef5904ece0152
[treasure3210] <treasure3210@gmail.com>
2023-12-26 18:46:19 -0700

    Extract the actual types for tint and category

--- ExpenseTracker/Model/Transaction.swift
+++ ExpenseTracker/Model/Transaction.swift
@@ -31,4 +31,12 @@ class Transaction: Identifiable {
     var color: Color {
         return tints.first(where: { $0.color == tintColor })?.value ?? appTint
     }
+    
+    var tint: TintColor? {
+        return tints.first(where: { $0.color == tintColor })
+    }
+    
+    var rawCategory: Category? {
+        return Category.allCases.first(where: { category == $0.rawValue })
+    }
 }
