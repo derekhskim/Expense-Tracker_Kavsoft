5c83e3766a33bc5c85de67a2d22ce596b277e0eb
[treasure3210] <treasure3210@gmail.com>
2023-12-28 21:45:17 -0700

    Transaction filter implementation so it matches given category, otherwise return all transactions that contain the searchText

--- ExpenseTracker/View/Helpers/FilterTransactionsView.swift
+++ ExpenseTracker/View/Helpers/FilterTransactionsView.swift
@@ -13,11 +13,20 @@ struct FilterTransactionsView<Content: View>: View {
     var content: ([Transaction]) -> Content
     
     @Query(animation: .snappy) private var transactions: [Transaction]
-    init(category: Category?, searchText: String) {
+    init(category: Category?, searchText: String, @ViewBuilder content: @escaping ([Transaction]) -> Content) {
         /// Custom Predicate
+        let rawValue = category?.rawValue ?? ""
         let predicate = #Predicate<Transaction> { transaction in
-            return transaction.title.localizedStandardContains(searchText) || transaction.remarks.localizedStandardContains(searchText)
+            return (transaction.title.localizedStandardContains(searchText) || transaction.remarks.localizedStandardContains(searchText)) && (rawValue.isEmpty ? true : transaction.category == rawValue)
         }
+        
+        _transactions = Query(
+            filter: predicate,
+            sort: [SortDescriptor(\Transaction.dateAdded, order: .reverse)],
+            animation: .snappy
+        )
+        
+        self.content = content
     }
     
     var body: some View {
