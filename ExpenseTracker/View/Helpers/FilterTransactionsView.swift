a0e8282fc4a7fe0499f3aac2ba28d362087165be
[treasure3210] <treasure3210@gmail.com>
2023-12-31 19:04:19 -0700

    Create a custom initializer for optional usage for date filtering

--- ExpenseTracker/View/Helpers/FilterTransactionsView.swift
+++ ExpenseTracker/View/Helpers/FilterTransactionsView.swift
@@ -43,6 +43,23 @@ struct FilterTransactionsView<Content: View>: View {
         self.content = content
     }
     
+    /// Optional For Customized Usage
+    init(startDate: Date, endDate: Date, category: Category?, @ViewBuilder content: @escaping ([Transaction]) -> Content) {
+        /// Custom Predicate
+        let rawValue = category?.rawValue ?? ""
+        let predicate = #Predicate<Transaction> { transaction in
+            return transaction.dateAdded >= startDate && transaction.dateAdded <= endDate && (rawValue.isEmpty ? true : transaction.category == rawValue)
+        }
+        
+        _transactions = Query(
+            filter: predicate,
+            sort: [SortDescriptor(\Transaction.dateAdded, order: .reverse)],
+            animation: .snappy
+        )
+        
+        self.content = content
+    }
+    
     var body: some View {
         content(transactions)
     }
