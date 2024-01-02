ebf630a1d4735c7ff8cbaec58fa319b5ed13841a
[treasure3210] <treasure3210@gmail.com>
2024-01-01 19:14:04 -0700

    Create a BarMark graph by iterating chartGroups

--- ExpenseTracker/View/TabViews/Graphs.swift
+++ ExpenseTracker/View/TabViews/Graphs.swift
@@ -10,10 +10,26 @@ import Charts
 import SwiftData
 
 struct Graphs: View {
+    /// View Properties
+    @Query(animation: .snappy) private var transactions: [Transaction]
+    @State private var chartGroups: [ChartGroup] = []
     var body: some View {
         ScrollView(.vertical) {
             LazyVStack(spacing: 10) {
                 /// Chart View
+                Chart {
+                    ForEach(chartGroups) { group in
+                        ForEach(group.categories) { chart in
+                            BarMark(
+                                x: .value("Month", format(date: group.date, format: "MMM yy")),
+                                y: .value(chart.category.rawValue, chart.totalValue),
+                                width: 20
+                            )
+                            .position(by: .value("Category", chart.category.rawValue), axis: .horizontal)
+                            .foregroundStyle(by: .value("Category", chart.category.rawValue))
+                        }
+                    }
+                }
             }
         }
     }
