070b6abebd8414c4fe2b5b1fd4ddd64781118aed
[treasure3210] <treasure3210@gmail.com>
2024-01-01 19:08:22 -0700

    Create a ChartModel to represent a graph that will show each month's total transactions based on categories

---
+++ ExpenseTracker/Model/ChartModel.swift
@@ -0,0 +1,22 @@
+//
+//  ChartModel.swift
+//  ExpenseTracker
+//
+//  Created by Derek Kim on 2024-01-01.
+//
+
+import SwiftUI
+
+struct ChartGroup: Identifiable {
+    let id: UUID = .init()
+    var date: Date
+    var categories: [ChartCategory]
+    var totalIncome: Double
+    var totalExpense: Double
+}
+
+struct ChartCategory: Identifiable {
+    let id: UUID = .init()
+    var totalValue: Double
+    var category: Category
+}
