6aa87908f3de1fb6cfe4ac1146deb70552c8ab34
[treasure3210] <treasure3210@gmail.com>
2024-01-01 22:30:55 -0700

    Update chart with groups sorted by date on main thread

--- ExpenseTracker/View/TabViews/Graphs.swift
+++ ExpenseTracker/View/TabViews/Graphs.swift
@@ -51,7 +51,47 @@ struct Graphs: View {
     }
     
     func createChartGroup() {
-        
+        Task.detached(priority: .high) {
+            let calendar = Calendar.current
+            
+            let groupedByDate = Dictionary(grouping: transactions) { transaction in
+                let components = calendar.dateComponents([.month, .year], from: transaction.dateAdded)
+                
+                return components
+            }
+            
+            /// Sorting Groups by Date
+            let sortedGroups = groupedByDate.sorted {
+                let date1 = calendar.date(from: $0.key) ?? .init()
+                let date2 = calendar.date(from: $1.key) ?? .init()
+                
+                return calendar.compare(date1, to: date2, toGranularity: .day) == .orderedDescending
+            }
+            
+            let chartGroups = sortedGroups.compactMap { dict -> ChartGroup? in
+                let date = calendar.date(from: dict.key) ?? .init()
+                let income = dict.value.filter({ $0.category == Category.income.rawValue })
+                let expense = dict.value.filter({ $0.category == Category.expense.rawValue })
+                
+                let incomeTotalValue = total(income, category: .income)
+                let expenseTotalValue = total(expense, category: .expense)
+                
+                return .init(
+                    date: date,
+                    categories: [
+                        .init(totalValue: incomeTotalValue, category: .income),
+                        .init(totalValue: expenseTotalValue, category: .expense)
+                    ],
+                    totalIncome: incomeTotalValue,
+                    totalExpense: expenseTotalValue
+                )
+            }
+            
+            /// UI must be updated on Main Thread
+            await MainActor.run {
+                self.chartGroups = chartGroups
+            }
+        }
     }
 }
 
