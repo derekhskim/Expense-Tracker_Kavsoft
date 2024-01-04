9996afdade8de3914e4bdbde48e2912bd52a0ea5
[treasure3210] <treasure3210@gmail.com>
2024-01-04 00:12:30 -0700

    Creation of Widget Extension Target

---
+++ StatsCardView/StatsCardView.swift
@@ -0,0 +1,63 @@
+//
+//  StatsCardView.swift
+//  StatsCardView
+//
+//  Created by Derek Kim on 2024-01-04.
+//
+
+import WidgetKit
+import SwiftUI
+
+struct Provider: TimelineProvider {
+    func placeholder(in context: Context) -> WidgetEntry {
+        WidgetEntry(date: Date())
+    }
+
+    func getSnapshot(in context: Context, completion: @escaping (WidgetEntry) -> ()) {
+        let entry = WidgetEntry(date: Date())
+        completion(entry)
+    }
+
+    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        var entries: [WidgetEntry] = []
+        
+        entries.append(.init(date: .now))
+
+        let timeline = Timeline(entries: entries, policy: .atEnd)
+        completion(timeline)
+    }
+}
+
+struct WidgetEntry: TimelineEntry {
+    let date: Date
+}
+
+struct StatsCardViewEntryView : View {
+    var entry: Provider.Entry
+
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+struct StatsCardView: Widget {
+    let kind: String = "StatsCardView"
+
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+            StatsCardViewEntryView(entry: entry)
+                .containerBackground(.fill.tertiary, for: .widget)
+        }
+        .contentMarginsDisabled()
+        .configurationDisplayName("Expense Widget")
+        .description("This is a Card View Widget for Expense Tracker Application.")
+    }
+}
+
+#Preview(as: .systemSmall) {
+    StatsCardView()
+} timeline: {
+    WidgetEntry(date: .now)
+}
---
+++ StatsCardView/StatsCardViewBundle.swift
@@ -0,0 +1,16 @@
+//
+//  StatsCardViewBundle.swift
+//  StatsCardView
+//
+//  Created by Derek Kim on 2024-01-04.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct StatsCardViewBundle: WidgetBundle {
+    var body: some Widget {
+        StatsCardView()
+    }
+}
